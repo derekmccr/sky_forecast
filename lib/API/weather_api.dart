e6968c30e911a95efaf66324dfea2dad45cdc718
derekM <derekmcc@knights.ucf.edu>
2019-06-28 15:05:11 -0400

    Update weather_api.dart
    refactored some code in weather_api

--- lib/API/weather_api.dart
+++ lib/API/weather_api.dart
@@ -6,13 +6,13 @@ import '../Models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 Future<Weather> fetchPost() async {
-  final response =
-  await http.get("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=44f9f099f38f499d40fc9ae277aabe33");
+  final response = await http.get("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=44f9f099f38f499d40fc9ae277aabe33");
 
   if (response.statusCode == 200) {
     // If the call to the server was successful, parse the JSON.
     return Weather.fromJson(json.decode(response.body));
-  } else {
+  }
+  else {
     // If that call was not successful, throw an error.
     throw Exception('Failed to load post');
   }
