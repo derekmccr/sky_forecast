440ddbcf2d74690d9fed13dfed13b7f1c6eabd39
derekM <derekmcc@knights.ucf.edu>
2019-07-17 12:38:40 -0400

    add dynamicTheme & set up preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'Mocks/location_mocks.dart';
 import 'ui_elements/location_list.dart';
+import 'package:preferences/preferences.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 
-void main() => runApp(MyApp());
+main() async {
+  await PrefService.init(prefix: 'pref_');
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   //load mock data
@@ -10,22 +16,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sky Forecast',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return DynamicTheme(
+      defaultBrightness: Brightness.light,
+      data: (brightness) => new ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: brightness,
       ),
-      //home: LocationListView(),
-      home: LocationList(mockLocations),
+      themedWidgetBuilder: (context, theme) {
+        return MaterialApp(
+          title: "Sky Forecast",
+          theme: theme,
+          //home: LocationListView(),
+          home: LocationList(mockLocations),
+        );
+      },
     );
   }
 }
