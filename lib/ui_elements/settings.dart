1299acf95ebc82e184f4fab1e7c46a332b2080f8
derekM <derekmcc@knights.ucf.edu>
2019-07-18 13:06:59 -0400

    settings page change to stateless
    current app runs successful. previous run showed error when having settings page as stateful

--- lib/ui_elements/settings.dart
+++ lib/ui_elements/settings.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:preferences/preferences.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
 
-class SettingsPage extends StatefulWidget {
+//stateful
+/*class SettingsPage extends StatefulWidget {
   SettingsPage({Key key}) : super(key: key);
 
   @override
@@ -46,6 +47,50 @@ class _SettingsPageState extends State<SettingsPage>{
           onSelect: () {
             DynamicTheme.of(context).setBrightness(Brightness.dark);
           }
+        ),
+      ]),
+    );
+  }
+}*/
+
+//stateless
+class SettingsPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: PreferencePage([
+        PreferenceTitle("Temperature Metric"),
+        RadioPreference(
+          "fahrenheit",
+          "f",
+          "temp_metric",
+          isDefault: true,
+        ),
+        RadioPreference(
+          "celsius",
+          "c",
+          "temp_metric",
+        ),
+        PreferenceTitle('Personalization'),
+        RadioPreference(
+          'Light Theme',
+          'light',
+          'ui_theme',
+          isDefault: true,
+          onSelect: (){
+            DynamicTheme.of(context).setBrightness(Brightness.light);
+          },
+        ),
+        RadioPreference(
+            'Dark Theme',
+            'dark',
+            'ui_theme',
+            onSelect: () {
+              DynamicTheme.of(context).setBrightness(Brightness.dark);
+            }
         ),
       ]),
     );
